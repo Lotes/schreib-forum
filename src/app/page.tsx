6dce039dac3573886bb96ed99d1296a791b4be40
Markus  Rudolph <markhoernchen@gmx.de>
2025-01-19 21:46:16 +0100

    Quick save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,156 @@
-import Image from "next/image";
+"use client";
+
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import CopyAll from "@mui/icons-material/CopyAll";
+import { makeDiff } from "@sanity/diff-match-patch";
+import { TagNode, parse } from "@bbob/parser";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tab, setTab] = useState(1);
+  const [oldText, setOldText] = useState("");
+  const [newText, setNewText] = useState("");
+  const [diffText, setDiffText] = useState<string>("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setTab(newValue);
+  };
+
+  useEffect(() => {
+    const diffs = makeDiff(oldText, newText);
+    setDiffText(
+      diffs
+        .map(([type, content]) => {
+          if (type < 0) {
+            return `[rot]${content}[/rot]`;
+          } else if (type > 0) {
+            return `[gruen]${content}[/gruen]`;
+          } else {
+            return content;
+          }
+        })
+        .join("")
+    );
+  }, [oldText, newText]);
+
+  return (
+    <div className="flex flex-col min-w-[50%] max-w-full h-full items-center justify-items-center p-8 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl">Textdifferenz</h1>
+      <main className="flex flex-col items-center w-full h-full">
+        <Box className="w-full h-dvh border-gray-300 border-[1px] flex flex-col">
+          <Tabs value={tab} onChange={handleChange} className="w-full">
+            <Tab label="Alter Text" value={1} />
+            <Tab label="Neuer Text" value={2} />
+            <Tab label="Vorschau" value={3} />
+          </Tabs>
+          {tab === 1 && (
+            <Box className="m-2 flex flex-col flex-grow items-end">
+              <textarea
+                className="w-full h-full"
+                placeholder="Bitte füge hier die alte Fassung deines Textes ein."
+                value={oldText}
+                onChange={(e) => setOldText(e.currentTarget.value)}
+              />
+              <Button
+                className="w-1/2"
+                variant="contained"
+                onClick={() => setTab(2)}
+              >
+                Weiter
+              </Button>
+            </Box>
+          )}
+          {tab === 2 && (
+            <Box className="m-2 flex flex-grow flex-col">
+              <textarea
+                className="w-full h-full"
+                placeholder="Bitte füge hier die neue Fassung deines Textes ein."
+                value={newText}
+                onChange={(e) => setNewText(e.currentTarget.value)}
+              />
+              <Box className="gap-2 flex flex-row">
+                <Button
+                  className="w-1/2"
+                  variant="outlined"
+                  onClick={() => setTab(1)}
+                >
+                  Zurück
+                </Button>
+                <Button
+                  className="w-1/2"
+                  variant="contained"
+                  onClick={() => setTab(3)}
+                >
+                  Weiter
+                </Button>
+              </Box>
+            </Box>
+          )}
+          {tab === 3 && (
+            <Box className="m-2 flex flex-grow flex-col">
+              <div className="flex flex-col flex-grow">
+                <BBCode bbCode={diffText} />
+              </div>
+              <Box className="gap-2 flex flex-row">
+                <Button
+                  className="w-1/2"
+                  variant="outlined"
+                  onClick={() => setTab(2)}
+                >
+                  Zurück
+                </Button>
+                <Button
+                  className="w-1/2"
+                  variant="contained"
+                  onClick={() => navigator.clipboard.writeText(diffText)}
+                  startIcon={<CopyAll />}
+                >
+                  BBCode
+                </Button>
+              </Box>
+            </Box>
+          )}
+        </Box>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+      <footer className="flex items-center justify-center">
+        Made with ❤️ by
+        <a className="mx-1" href="https://github.com/Lotes" target="_blank">
+          Lotes
         </a>
       </footer>
     </div>
   );
+}
+
+function BBCode({ bbCode }: { bbCode: string }) {
+  const [ast, setAst] = useState<TagNode[]>([]);
+  useEffect(() => {
+    setAst(parse(bbCode, { onlyAllowTags: ["gruen", "rot"] }));
+  }, [bbCode]);
+  return (
+    <Box className="">
+      {ast.map((a, index) => {
+        switch (a.tag) {
+          case "rot":
+            return <span key={index} className="bg-red-300 inline-block">{breakify(a.content!.toString())}</span>;
+          case "gruen":
+            return (
+              <span key={index} className="bg-green-300 inline-block">{breakify(a.content!.toString())}</span>
+            );
+          default:
+            return (
+              <span key={index} className="inline-block">{breakify(a as unknown as string)}</span>
+            );
+        }
+      })}
+    </Box>
+  );
+}
+
+function breakify(text:string) {
+  return <>{text.split(/\n/g).map((t, i) => <span key={i}>{t}<br/></span>)}</>;
 }
