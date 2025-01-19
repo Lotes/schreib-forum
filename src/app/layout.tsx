d50a459aaebe61ad443b6e76f1c6f53b085d5915
Markus  Rudolph <markhoernchen@gmx.de>
2025-01-19 22:03:42 +0100

    Fix typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Schreibforum-Tools",
-  description: "Werkyeuge für deine Textarbeit.",
+  title: "Schreib-Forum-Tools",
+  description: "Werkzeuge für deine Textarbeit.",
 };
 
 export default function RootLayout({
